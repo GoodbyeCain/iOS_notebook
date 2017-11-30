d11922270865ba96ae1ede80ca733bb364ccc0e4
pan <2331838272@qq.com>
2017-11-30 22:37:43 +0800

    tmp

---
+++ main/开发/CoreAudio/ZPRingtone/ZPRingtone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZPRingtone
+//
+//  Created by zhoupan on 2017/11/30.
+//  Copyright © 2017年 zhoupan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ main/开发/CoreAudio/ZPRingtone/ZPRingtone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZPRingtone
+//
+//  Created by zhoupan on 2017/11/30.
+//  Copyright © 2017年 zhoupan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
